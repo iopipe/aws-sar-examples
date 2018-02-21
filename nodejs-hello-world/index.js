9d3aa37b4b849a4ad7681a1a27b39ad86ca4584f
Corey Light <coreylight@gmail.com>
2018-02-21 15:07:44 -0600

    call the iopipe func, shorter export

--- nodejs-hello-world/index.js
+++ nodejs-hello-world/index.js
@@ -2,8 +2,8 @@
  USAGE: set the IOPIPE_TOKEN env var to your project token
 */
 'use strict';
-const iopipe = require('@iopipe/iopipe');
+const iopipe = require('@iopipe/iopipe')();
 
-module.exports.handler = iopipe((event, context, callback) => {
+exports.handler = iopipe((event, context, callback) => {
   callback(null, "Hello World!");
 });
